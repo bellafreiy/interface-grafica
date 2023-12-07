0d4e7feca6fdc4256f912698718dd58e86bac6ea
bellafreiy <gabriellafreitas.g@gmail.com>
2023-12-07 20:35:27 -0300

    refatoramento 1

--- projeto-quiz/app/src/main/java/quizgame/menu/Pergunta.java
+++ projeto-quiz/app/src/main/java/quizgame/menu/Pergunta.java
@@ -6,21 +6,13 @@ import java.util.List;
 
 public class Pergunta {
     public String titulo;
-    public String areaDoConhecimento;
     private List<Alternativa> alternativas;
-
+    
     public Pergunta(String titulo) {
         this.titulo = titulo;
         this.alternativas = new ArrayList<>();
     }
 
-    // Construtor da classe Pergunta
-    public Pergunta(String titulo, String areaDoConhecimento) {
-        this.titulo = titulo;
-        this.areaDoConhecimento = areaDoConhecimento;
-        this.alternativas = new ArrayList<>();
-    }
-    // Métodos getter e setter para a lista de alternativas
     public List<Alternativa> getAlternativas() {
         return alternativas;
     }
@@ -29,7 +21,6 @@ public class Pergunta {
         this.alternativas = alternativas;
     }
 
-    // Métodos getter e setter para o título da pergunta
     public String getTitulo() {
         return titulo;
     }
@@ -37,15 +28,11 @@ public class Pergunta {
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-    public String getAreaDoConhecimento() {
-        return areaDoConhecimento;
-    }
 
     public void adicionarAlternativa(Alternativa alternativa) {
         alternativas.add(alternativa);
     }
 
-    // Método para verificar se a resposta do usuário está correta
     public boolean verificarResposta(String respostaUsuario) {
         for (Alternativa alternativa : alternativas) {
             if (alternativa.getOpcao().equalsIgnoreCase(respostaUsuario) && alternativa.isOpcaoCorreta()) {
